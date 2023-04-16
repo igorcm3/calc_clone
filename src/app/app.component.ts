b455fecf155836e71d449e4d5408178336bb8737
Igor Corona de Matos <32557238+igorcm3@users.noreply.github.com>
2023-04-16 19:09:41 -0300

    feat: implemented buttons and pass button info to app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+  @ViewChild('input_view')
+  inputView!: ElementRef;
+
+  btnClicked(btnValue: string) {
+    console.log(`recebi o valor ${btnValue} no componente`);
+    this.inputView.nativeElement.value = btnValue;
+  }
+
   title = 'calc_clone';
 }
--- src/app/components/calc-button/calc-button.component.ts
+++ src/app/components/calc-button/calc-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-calc-button',
@@ -12,5 +12,13 @@ export class CalcButtonComponent {
 
   @Input()
   buttonType: string | undefined;
+
+  @Output() btnClicked = new EventEmitter<string>();
+
+
+  handleClick(){
+    console.log(`botão ${this.text} clicado`);
+    this.btnClicked.emit(this.text);
+  }
 
 }
